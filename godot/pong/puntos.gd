199317db1df33579922e4397af4a46911ecf0289
alevera09-dev <aleveralopez09@hotmail.com>
2025-12-01 16:12:06 -0500

    Delete godot directory

--- godot/pong/puntos.gd
+++
@@ -1,9 +0,0 @@
-class_name Puntos
-extends Node
-
-var puntos_player_1: int = 0
-var puntos_player_2: int = 0
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
